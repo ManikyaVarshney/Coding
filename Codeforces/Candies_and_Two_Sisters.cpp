07997cc898d6ea58d840ab8738b719a6aff948f2
Manikya Varshney <115883858+ManikyaVarshney@users.noreply.github.com>
2023-10-27 00:12:40 +0530

    Add files via upload

---
+++ Codeforces/Candies_and_Two_Sisters.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+            long int testcase;
+            cin>>testcase;
+            while(testcase!=0){
+                long long n;
+                cin>>n;
+                cout<<(n-1)/2<<endl;
+                testcase--;
+            }
+    return 0;
+}
